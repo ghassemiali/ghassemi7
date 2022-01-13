5f18ab8df38c1e30d0cc46c01f0fd7c8aa5ed7c9
ghassemiali <agh101981@gmail.com>
2022-01-14 00:30:43 +0330

    8-2

--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from blog.models import Post, Tag
 from website.models import Contact
+from website.forms import NameForm
+from django.http import HttpResponse
 
 def home_view(request):
     return render(request, 'website/index.html')
@@ -16,14 +18,15 @@ def elements_view(request):
 
 def test_view(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        subject = request.POST.get('subject')
-        message = request.POST.get('message')
-        c = Contact()
-        c.name = name
-        c.email = email
-        c.subject = subject
-        c.message = message
-        c.save()
-    return render(request, 'test.html', {})
+        form = NameForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            subject = form.cleaned_data['subject']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            print(name, subject, email, message)
+        else:
+            return HttpResponse('not valid')
+
+    form = NameForm()
+    return render(request, 'test.html', {'form': form})
